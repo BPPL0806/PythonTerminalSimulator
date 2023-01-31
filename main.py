3806f536bb36ddeb244a24fbd94b0fa6ad33db1f
BPPL0806 <84276537+BPPL0806@users.noreply.github.com>
2023-01-31 20:33:45 +0100

    Comments added as some sort of documentation

--- main.py
+++ main.py
@@ -3,23 +3,25 @@
 
 import subprocess, getpass, os, platform
 
-username = getpass.getuser()
+username = getpass.getuser() #Username that is used on local machine
 
+#Tree command function
 def display_tree(path, level):
     for i in os.listdir(path):
         print("  " * level + "├───" + i)
         current_path = os.path.join(path, i)
         if os.path.isdir(current_path):
             display_tree(current_path, level + 1)
-
+#clear command function
 def clear_terminal():
     if platform.system() == "Windows":
         subprocess.call("cls", shell=True)
     else:
         subprocess.call("clear", shell=True)
-
 def main():
+    #Clear local terminal on startup
     clear_terminal()
+    #App's main loop
     while True:
         command = input("["+username+"@"+platform.system()+"]$ ").split(" && ")
         for cmd in command:
@@ -38,20 +40,25 @@ def main():
                         os.chdir(cmd[1])
                     except FileNotFoundError:
                         print("cd: {}: No such file or directory".format(cmd[1]))
+            #pwd command showing currently opened directory
             elif cmd[0] == "pwd":
                 print(os.getcwd())
+            #whoami command displaying username
             elif cmd[0] == "whoami":
                 print(username)
+            #mkdir command creating folders
             elif cmd[0] == "mkdir":
                 if len(cmd) == 1:
                     print("mkdir: missing operand")
                 else:
                     os.mkdir(cmd[1])
+            #touch command creating files
             elif cmd[0] == "touch":
                 if len(cmd) == 1:
                     print("touch: missing operand")
                 else:
                     open(cmd[1], 'a').close()
+            #color command for changing terminal's colors
             elif cmd[0] == "color":
                 if len(cmd) == 1:
                     print("color: missing operand")
@@ -60,6 +67,7 @@ def main():
                         os.system("color {0}".format(cmd[1]))
                     else:
                         print("{}: command not found".format(cmd[0]))
+            #help command for showing all available commands
             elif cmd[0] == "help":
                 print("List of commands:")
                 print("ls - list files in current directory")
@@ -73,13 +81,17 @@ def main():
                 print("exit - close the app")
                 print("color - changes colors of terminal")
                 print("whoami - display your username")
+            #tree command displaying structure of currently opened directory
             elif cmd[0] == "tree":
                 print("." + os.sep)
                 display_tree(os.getcwd(), 1)
+            #clear command clearing simulated terminal
             elif cmd[0] == "clear":
                 clear_terminal()
+            #exit command that closes app
             elif cmd[0] == "exit":
                 return
+            #else that executes if non-existent command is typed in
             else:
                 print("{}: command not found".format(cmd[0]))
 
